5fa420e2fb0244407cb675de609a01af9c9d6234
Ava <avanastar3@gmail.com>
2024-10-26 16:16:22 -0300

    Initial commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+function Home() {
+  return (
+    <>
+      <div className="hero bg-base-200 min-h-screen">
+        <div className="hero-content text-center">
+          <div className="max-w-md">
+            <h1 className="text-5xl font-bold">Daisy Template</h1>
+            <p className="py-6">
+              this is a template for daisyUI, a component library for tailwind
+            </p>
+            <a
+              href="https://github.com/Avery-Rose/DaisyTemplate"
+              target="_blank"
+              className="btn btn-primary"
+            >
+              View on GitHub
+            </a>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Home;
